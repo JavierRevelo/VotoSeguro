fbcf7ead82dec0024e4b1db4d53502723e0627cc
Javier Revelo <shavi.sprint@gmail.com>
2024-03-01 12:56:22 -0500

    Repositorio voto seguro

---
+++ Models/VotesRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace VotoSeguro.Models
+{
+    public class VotesRecord
+    {
+
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int CantidadVotos { get; set; }
+    }
+}
